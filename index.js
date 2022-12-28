0b339f3f2880182223192ae6d7fd92f740a95193
officialgio121 <officialgio121@gmail.com>
2022-12-28 17:05:01 -0600

    Added additional accessbility and small tweaks in the main page

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const nav = document.querySelector(".primary-navigation");
 const navToggle = document.querySelector(".mobile-nav--toggle");
 
-// when someone clicks the hamburger menu
 navToggle.addEventListener("click", () => {
     const visible = nav.getAttribute("data-visible");
 
@@ -14,7 +13,3 @@ navToggle.addEventListener("click", () => {
         navToggle.setAttribute("aria-expanded", false);
     }
 });
-
-
-// if the menu is closed, open it
-// if the menu is open, close it
