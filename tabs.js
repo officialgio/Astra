7fb7c4970dba77ceebc5e20915b922724dfdbd50
officialgio121 <officialgio121@gmail.com>
2023-01-02 20:30:25 -0600

    Completed majority of the project w/o technology html

--- tabs.js
+++ tabs.js
@@ -1,8 +1,9 @@
-const tabList = document.querySelector('[role="tablist"]');
-const tabs = tabList.querySelectorAll('[role="tab"]');
+const tabList = document.querySelector('[role="tablist"]'); // div for btns
+const tabs = tabList.querySelectorAll('[role="tab"]'); // btn
 
 let tabFocus = 0;
 
+// Accessibility stuff
 const changeTabFocus = (e) => {
     const keydownLeft = 37;
     const keydownRight = 39;
@@ -10,24 +11,25 @@ const changeTabFocus = (e) => {
     // change the tabindex of the current tab to -1
     // if the right key is pushed, move to the next tab on the right
     // if the left key is pushed, move to the next tab on the left
-    if (e.keyCode === keydownLeft || e.keyCode === keydownLeft) {
-        tabs[tabFocus].setAttribute('tabindex', -1);
-    }
+    if (e.keyCode === keydownLeft || e.keyCode === keydownRight) {
+        tabs[tabFocus].setAttribute("tabindex", -1);
 
-    if (e === keydownRight) {
-        tabFocus++;
-        if (tabFocus >= tabs.length) tabFocus = 0;
-    }
+        if (e.keyCode === keydownRight) {
+            tabFocus++;
+            if (tabFocus >= tabs.length) {
+                tabFocus = 0;
+            }
+        } else if (e.keyCode === keydownLeft) {
+            tabFocus--;
+            if (tabFocus < 0) {
+                tabFocus = tabs.length - 1;
+            }
+        }
 
-    if (e.keyCode === keydownLeft) {
-        tabFocus--;
-        if (tabFocus < 0) tabFocus = tabs.length - 1;
+        tabs[tabFocus].setAttribute("tabindex", 0);
+        tabs[tabFocus].focus();
     }
-
-    tabs[tabFocus].setAttribute('tabindex', 0);
-    tabs[tabFocus].focus();
 }
-
 
 const changeTabPanel = (e) => {
     const targetTab = e.target;
@@ -37,21 +39,35 @@ const changeTabPanel = (e) => {
     const tabContainer = targetTab.parentNode; // div
     const mainContainer = tabContainer.parentNode; // main
 
-    // set them all to hidden and then remove hidden from the corresponding panel
-    mainContainer
-        .querySelectorAll('[role="tabpanel"]')
-        .forEach((panel) => panel.setAttribute("hidden", true));
+    // remove the aria-selected (T) to false when choosing a new tab
+    // Set the corresponding pressed tab to True.
+    // This will allow to change border colors
+    tabContainer
+        .querySelector('[aria-selected="true"]')
+        .setAttribute("aria-selected", false);
+    targetTab.setAttribute("aria-selected", true);
 
-    mainContainer.querySelector([`#${targetPanel}`]).removeAttribute('hidden');
+    // Initially set them to hidden state, then,
+    // Get it's attribute and remove hidden
+    hideContent(mainContainer, '[role="tabpanel"]');
+    showContent(mainContainer, [`#${targetPanel}`])
 
-    mainContainer
-        .querySelectorAll('picture')
-        .forEach((picture) => picture.setAttribute("hidden", true));
-
-    mainContainer.querySelector([`#${targetImage}`]).removeAttribute('hidden');
-
+    hideContent(mainContainer, 'picture');
+    showContent(mainContainer, [`#${targetImage}`]);
 }
 
+// This allows to hide the content that corresponds to the linked id attribute through aria-controls
+const hideContent = (parent, content) => {
+    parent
+        .querySelectorAll(content)
+        .forEach((item) => item.setAttribute("hidden", true));
+}
+
+const showContent = (parent, content) => {
+    parent.querySelector(content).removeAttribute('hidden');
+}
+
+// Invoke 
 tabList.addEventListener('keydown', changeTabFocus);
 tabs.forEach((tab) => {
     tab.addEventListener('click', changeTabPanel);
